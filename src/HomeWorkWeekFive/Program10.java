8775a97f97e9b884381c5c7d483cdfa5fb27b555
hiralyagnikqa <hiral.rajyaguru@gmail.com>
2023-03-05 17:28:19 +0000

    Program 2-9

--- src/HomeWorkWeekFive/Program10.java
+++ src/HomeWorkWeekFive/Program10.java
@@ -1,4 +1,17 @@
 package HomeWorkWeekFive;
-
+/*
+* Write a Java program that takes two numbers as input and display the
+product of two numbers.
+Test Data:
+Input first number: 25
+Input second number: 5
+Expected Output : 25 x 5 = 125
+* */
 public class Program10 {
+    public static void main(String[] args){
+        int a =25;
+        int b = 5;
+        System.out.println(a*b);
+
+    }
 }
