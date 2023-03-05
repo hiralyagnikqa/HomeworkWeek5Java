96b253d06d33a772b37e90774fb52e4b3da31527
hiralyagnikqa <hiral.rajyaguru@gmail.com>
2023-03-05 18:52:10 +0000

    Updating program 1 and 10

--- src/HomeWorkWeekFive/Program7.java
+++ src/HomeWorkWeekFive/Program7.java
@@ -3,6 +3,7 @@ package HomeWorkWeekFive;
 Write a Java program to print the sum of two numbers.
 Test Data: 74 + 36
 Expected Output: 110
+test
  */
 public class Program7 {
     public static void main(String[] args){
