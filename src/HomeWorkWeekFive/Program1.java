cecdab1d5b68cddb183ef723d0b48b0f86891c83
hiralyagnikqa <hiral.rajyaguru@gmail.com>
2023-03-05 18:35:01 +0000

    Updating program 1 and 10

--- src/HomeWorkWeekFive/Program1.java
+++ src/HomeWorkWeekFive/Program1.java
@@ -2,24 +2,28 @@ package HomeWorkWeekFive;
 
 public class Program1 {
     public static void main(String[] args) {
-        System.out.println("===== Bio Data =====");
-        System.out.print("Full Name: ");
-        System.out.println("Hiral Yagnik");
-        System.out.print("Address Line 1: ");
-        System.out.println("7 Testing Avneue");
-        System.out.print("Adderss Line 2: ");
-        System.out.println("Borehamwood");
-        System.out.print("Postcode: ");
-        System.out.println("PM1 2AM");
-        System.out.print("Phone Number: ");
-        System.out.println("+440123456789");
-        System.out.print("Email: ");
-        System.out.println("abc.xyz@gmail.com");
-        System.out.print("Dietary: ");
-        System.out.println("Vegetarian");
-        System.out.print("Favourite Holiday Destination: ");
-        System.out.println("Seaside/Mountains");
-        System.out.print("Things I Enjoy: ");
-        System.out.println("Drive/Visiting new places/Meeting new people");
+        System.out.println("*************** Bio Data ***************");
+        System.out.print("Full Name           ");
+        System.out.println(": Hiral Yagnik");
+        System.out.print("Address Line 1:     ");
+        System.out.println(": 7 Testing Avneue");
+        System.out.print("Adderss Line 2      ");
+        System.out.println(": Borehamwood");
+        System.out.print("Postcode            ");
+        System.out.println(": PM1 2AM");
+        System.out.print("Phone Number        ");
+        System.out.println(": +440123456789");
+        System.out.print("Email               ");
+        System.out.println(": abc.xyz@gmail.com");
+        System.out.print("Current Role        ");
+        System.out.println(": QA Engineer");
+        System.out.print("Starting Date       ");
+        System.out.println(": 01/08/2022");
+        System.out.print("Preferred work      ");
+        System.out.println(": Remote");
+        System.out.print("Diet                ");
+        System.out.println(": Vegetarian");
+        System.out.print("Holiday Destination ");
+        System.out.println(": Seaside/Mountains");
     }
 }
